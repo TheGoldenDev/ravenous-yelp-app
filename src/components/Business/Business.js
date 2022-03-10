4b3c61e2e6ada90bc35bb7e3ff47a5971fd218ef
TheGoldenDev <Spencer.uab@gmail.com>
2022-03-10 04:01:50 -0500

    Update Business.js
    added function to format phone numbers

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,6 +2,22 @@ import React from 'react';
 import './Business.css';
 
 class Business extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.formatPhoneNumber = this.formatPhoneNumber.bind(this);
+  }
+
+  //Changes the format of the phone number to be more readable.
+  formatPhoneNumber(phoneNumberString) {
+    const cleaned = ('' + phoneNumberString).replace(/\D/g, '');
+    const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+    if (match) {
+      return '(' + match[1] + ') ' + match[2] + '-' + match[3];
+    }
+    return null;
+  }
+
   render() {
     return (
       <div className='Business'>
@@ -17,7 +33,7 @@ class Business extends React.Component {
             <p>{this.props.business.address}</p>
             <p>{this.props.business.city}</p>
             <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
-            <p>{this.props.business.phone}</p>
+            <p>{formatPhoneNumber(this.props.business.phone)}</p>
           </div>
           <div className='Business-reviews'>
             <h3>{this.props.business.category.toUpperCase()}</h3>
